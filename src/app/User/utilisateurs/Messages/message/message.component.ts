fe071399f11c5cdca88e47ec8f7140c4962375e4
Chris <c.havrez@stag.technofuturtic.education>
2023-11-28 03:51:21 +0100

    Clean Code

--- src/app/User/utilisateurs/Messages/message/message.component.ts
+++ src/app/User/utilisateurs/Messages/message/message.component.ts
@@ -7,5 +7,30 @@ import { MessageInterface } from '../Interface/message.interface';
 })
 export class MessageComponent {
 
-  
+
+  messages = []; // Remplacer par le mécanisme approprié pour récupérer les messages
+  newMessage = '';
+  selectedFile: File | null = null;
+
+  constructor(/* Injection de dépendances */) {}
+
+  sendMessage() {
+    const messageData = {
+      text: this.newMessage,
+      image: this.selectedFile,
+    };
+    // Logique pour envoyer le message au backend
+    this.newMessage = '';
+    this.selectedFile = null;
+  }
+
+  onFileSelected(event: Event) {
+    // Logique pour gérer la sélection du fichier
+  }
+
+  getUrlFromSelectedFile() {
+    // Logique pour obtenir l'URL du fichier sélectionné
+  }
 }
+
+
