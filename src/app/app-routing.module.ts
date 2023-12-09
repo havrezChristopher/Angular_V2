c9c7ee9653e913836cc1c36cb77b15f709a606c6
Chris <c.havrez@stag.technofuturtic.education>
2023-12-09 18:31:42 +0100

    Clean .spec

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ReservationComponent } from './reservation/reservation.component';
 import { ContactComponent } from './contact/contact.component';
-// authentification
+//! ***************************************** authentification ***********************************
 import { LoginComponent } from './authentification/login/login.component';
 import { RegisterComponent } from './authentification/register/register.component';
 import { AuthGuard } from './authentification/Guard/auth.guard.guard';
-// Menu DashBoard
+//! ***************************************** authentification ***********************************
+//? ***********************************Menu DashBoard*********************************
 import { UtilisateursComponent } from './User/utilisateurs/utilisateurs.component';
 import { EvenementComponent } from './Evenements/evenement/evenement.component';
 // Message
@@ -17,31 +18,28 @@ import { BoiteDeReceptionComponent } from './User/utilisateurs/Message/boiteDeRe
 import { MesgroupComponent } from './User/utilisateurs/Groupe/mesgroup/mesgroup.component';
 // Paramettre
 import { ParametteComponent } from './User/utilisateurs/Paramettres/paramette/paramette.component';
-
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
+//? ***********************************Menu DashBoard*********************************
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'reservation', component: ReservationComponent },
   { path: 'about', component: AboutComponent },
-  { path: 'evenement', component: EvenementComponent },
+  { path: 'reservation', component: ReservationComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'evenement', component: EvenementComponent },
+  //! ***auth*** 
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  //! ***auth***
   // Guard Route proteger 
   { path: 'utilisateur/:id', component: UtilisateursComponent, canActivate: [AuthGuard] },
   { path: 'utilisateur/:id/mesgroupe', component: MesgroupComponent },
   { path: 'utilisateur/:id/boiteReception', component: BoiteDeReceptionComponent },
   { path: 'utilisateur/:id/paramettre', component: ParametteComponent },
-  // dashBoard 
-
 
   //! a mettre a la fin sinon il ce met en page par default! 
   { path: '**', component: PageNotFoundComponent },
-
-
 
 ];
 
